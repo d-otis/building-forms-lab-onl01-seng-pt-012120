16dff2c15ceefa1e903cdc8c09dffa3d9ef1babd
d-otis <dan@dan-foley.com>
2020-10-14 17:13:51 -0400

    Done.

--- src/reducers/manageBand.js
+++ src/reducers/manageBand.js
@@ -1,5 +1,24 @@
 export default function manageBand(state = {
   bands: [],
 }, action) {
-  return state
+
+  switch (action.type) {
+    case 'ADD_BAND':
+      // return {...state, bands: [...state.bands, action.band]}
+      return {...state, bands: state.bands.concat({name: action.band})}
+
+    default:
+      return state
+  }  
 };
+
+// Tests want it to look like this:
+// state = {
+//   bands: [
+//     {name: 'Hello'},
+//     {name: 'Matracia'},
+//     {name: 'The Moon Baby'}
+//   ]
+// }
+
+// expect(store.getState().bands[0].name).to.equal("Hello")
